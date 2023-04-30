5812a3d04446a131b6883d90ca208233dc10b8e5
yeoni sugar <rkdgkdus522@naver.com>
2023-05-01 01:49:36 +0900

    [ADD] : 공통 레이아웃 - Footer 추가

--- src/components/common/AppFooter.vue
+++ src/components/common/AppFooter.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>Footer</div>
+  <footer class="footer">
+    <div class="footer-description">
+      <div class="copyright">Copyright (C) 2023 WEB Project.</div>
+      <div class="developers">
+        <div class="row">
+          <div class="title">Made By</div>
+          <p>{{ developer }}</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {
   name: 'AppFooter',
   data() {
-    return {};
+    return {
+      developer: 'Lydia',
+    };
   },
-  methods: {},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/common/footer.scss';
+</style>
