1a1b5b5c6ddc7515b11820add224334b95cb6cab
yeoni sugar <rkdgkdus522@naver.com>
2023-05-01 01:38:48 +0900

    [ADD] : 공통 레이아웃 - Header 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
-    <router-view />
+    <AppHeader />
+    <main>
+      <router-view />
+    </main>
+    <AppFooter />
   </div>
 </template>
+
+<script>
+import AppHeader from '@/components/common/AppHeader.vue';
+import AppFooter from '@/components/common/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
 
 <style>
 @import '@/styles/style.scss';
---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Footer</div>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
