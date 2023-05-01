83e69d8d8b194f33491f37077a0711cc9e5b8fca
yeoni sugar <rkdgkdus522@naver.com>
2023-05-01 12:14:05 +0900

    [ADD] : 공통 컴포넌트 제작 - CheckBox

--- src/components/common/CheckBox.vue
+++ src/components/common/CheckBox.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>CheckBox</div>
+  <div class="checkbox">
+    <input
+      type="checkbox"
+      class="checkbox__input"
+      :checked="checked"
+      :disabled="disabled"
+      ref="checkbox"
+      @change="check"
+    />
+    <span class="checkbox__content">{{ text }}</span>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'CheckBox',
-  data() {
-    return {};
+  props: {
+    text: {
+      type: String,
+      default: '요소',
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
-  methods: {},
+  watch: {
+    disabled() {
+      if (!this.disabled) {
+        this.$refs.checkbox.checked = false;
+      }
+    },
+  },
+  methods: {
+    check(e) {
+      this.$emit('check', e.target.checked, this.text);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/common/checkBox.scss';
+</style>
