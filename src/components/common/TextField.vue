f32d363568dac20728df59e7464f966ca39e2b37
yeoni sugar <rkdgkdus522@naver.com>
2023-05-02 23:12:52 +0900

    [ADD] : 공통 컴포넌트 제작 - Text Field

--- src/components/common/TextField.vue
+++ src/components/common/TextField.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>TextField</div>
+  <div class="text-container">
+    <input
+      class="text-field__input"
+      :type="type"
+      :placeholder="placeholder"
+      :class="[{ error: isError }, { focused: isFocused }, color]"
+      :value="inputValue"
+      @input="handleInput"
+    />
+    <div
+      class="text-field__message"
+      :class="[{ error: isError }, { success: isSuccess }]"
+    >
+      <span v-if="isSuccess || isError">{{ message }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'TextField',
-  data() {
-    return {};
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    isError: {
+      type: Boolean,
+      default: false,
+    },
+    isSuccess: {
+      type: Boolean,
+      default: false,
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    color: {
+      type: String,
+      default: 'surface',
+    },
   },
-  methods: {},
+  data() {
+    return {
+      inputValue: '',
+      isFocused: true,
+    };
+  },
+  methods: {
+    handleInput(event) {
+      this.inputValue = event.target.value;
+      this.emitInput();
+    },
+    emitInput() {
+      this.$emit('input', this.inputValue);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/styles/common/textField.scss';
+</style>
