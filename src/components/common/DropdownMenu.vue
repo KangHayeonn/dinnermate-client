f9086675ffe34ffccb7e0fb2cbb08db54c5a3f69
yeoni sugar <rkdgkdus522@naver.com>
2023-05-21 00:20:29 +0900

    [ADD] : 공통 컴포넌트 - DropdownMenu 제작

---
+++ src/components/common/DropdownMenu.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="dropdown" :class="{ opened: isOpen }">
+      <div
+        type="button"
+        class="dropdown-button"
+        :class="{ opened: isOpen }"
+        @click="onMenuClick"
+        v-click-outside="onClickOutSide"
+      >
+        <span>{{ selectedMenuItem }}</span>
+        <img
+          alt="arrow"
+          class="dropdown-button__arrow"
+          :class="{ opened: isOpen }"
+        />
+      </div>
+      <div v-show="isOpen" class="dropdown-menu">
+        <div
+          v-for="(item, index) in menuItems"
+          :key="index"
+          class="dropdown-menu__item"
+          @click="handleClick(item)"
+        >
+          {{ item }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownMenu',
+  props: {
+    menuItems: {
+      type: Array,
+      default: () => {
+        return ['주소', '회사'];
+      },
+    },
+  },
+  data() {
+    return {
+      isOpen: false,
+      selectedMenuItem: '주소',
+    };
+  },
+  methods: {
+    onMenuClick() {
+      this.isOpen = !this.isOpen;
+    },
+    onClickOutside() {
+      this.isOpen = false;
+    },
+    handleClick(item) {
+      this.selectedMenuItem = item;
+      this.isOpen = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/common/dropdownMenu.scss';
+</style>
